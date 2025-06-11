d07b33024b5788af2f3df83da5b1e25c22228a91
Matthew Maugeri <53546314+MattyMaugeri@users.noreply.github.com>
2025-06-11 15:26:16 +1000

    Completed all functions up to toString()

--- LinkedList.js
+++ LinkedList.js
@@ -35,4 +35,113 @@ export class LinkedList {
         this.head = node;
 
     }
+
+    size() {
+        let count = 0;
+        if (this.head === null) return 0;
+
+        let current = this.head;
+        while (current) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+
+    first() {
+        if (this.head === null) return null;
+
+        return this.head;
+    }
+
+    last() {
+        if (this.head === null) return null;
+
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next
+        }
+
+        return current;
+    }
+
+    at(index) {
+        if (this.head === null) return null;
+
+        let current = this.head;
+
+        let count = 0;
+        while (current !== null && count !== index) {
+            current = current.next;
+            count++;
+        }
+
+        if (current !== null) {
+            return current;
+        }
+
+        return null;
+    }
+
+    pop() {
+        if (this.head === null) return null;
+
+        let current = this.head;
+        let prev = null;
+
+        while (current.next !== null) {
+            prev = current;
+            current = current.next;
+        }
+
+        prev.next = null;
+
+    }
+
+    contains(value) {
+        if (this.head === null) return false;
+
+        let curr = this.head;
+        while (curr) {
+            if (curr.data === value) {
+                return true;
+            }
+            curr = curr.next;
+        }
+        return false;
+    }
+
+    findIndexOf(value) {
+        if (this.head === null) return null;
+
+        let curr = this.head;
+        let idx = 0;
+
+        while (curr) {
+            if (curr.data === value) {
+                return idx;
+            }
+            curr = curr.next;
+            idx++;
+        }
+        return null;
+    }
+
+    toString() {
+        if (this.head === null) return '';
+
+        let string = '';
+        let curr = this.head;
+
+        while (curr) {
+            if (curr.next === null) {
+                string += `( ${curr.data} ) -> null`;
+                break;
+            }
+            string += `( ${curr.data} ) -> `;
+            curr = curr.next;
+        }
+        return string;
+    }
 }
--- main.js
+++ main.js
@@ -9,9 +9,24 @@ node1.next = node2;
 
 
 list.append(500);
-
-list.print();
-
 list.prepend(1000);
 
+console.log('Complete List: ');
 list.print();
+
+console.log('List size: ', list.size());
+
+console.log('First node: ', list.first());
+
+console.log('Last node: ', list.last());
+
+let idx = 0;
+console.log(`Node at index ${idx}: `, list.at(idx));
+
+list.print();
+
+console.log(list.contains(500));
+
+console.log(list.findIndexOf(1000));
+
+console.log(list.toString());
